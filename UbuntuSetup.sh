39c3e8a2362262870a5d9191c1d90385a63e9ddd
tylernancecs <65039391+tylernancecs@users.noreply.github.com>
2021-09-16 10:34:39 -0500

    Upgraded to 4.2

--- UbuntuSetup.sh
+++ UbuntuSetup.sh
@@ -6,8 +6,8 @@
 # Installs Mongodb
 function InstallMongoDB ()
 {
-	wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
-	echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+	wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+	echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 	sudo apt-get update
 	sudo apt-get install -y mongodb-org
 }
@@ -41,7 +41,7 @@ systemLog:
 # network interfaces
 net:
   port: 27017
-
+  bindIp: 0.0.0.0
 #Comment out this section if not replicating
 replication:
   oplogSizeMB: 10240
