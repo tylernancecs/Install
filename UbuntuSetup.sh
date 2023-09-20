5bb03a3da156f9e6fc15af339a5084fdaa26eed1
tylernancecs <65039391+tylernancecs@users.noreply.github.com>
2023-09-20 13:12:25 -0500

    Update UbuntuSetup.sh

--- UbuntuSetup.sh
+++ UbuntuSetup.sh
@@ -5,8 +5,11 @@
 
 # Installs Mongodb
 function InstallMongoDB ()
-	wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
-	echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+	sudo apt-get install gnupg curl
+	curl -fsSL https://pgp.mongodb.com/server-5.0.asc | \
+		sudo gpg -o /usr/share/keyrings/mongodb-server-5.0.gpg \
+   		--dearmor
+	echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-5.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 	sudo apt-get update
 	sudo apt-get install -y mongodb-org
 }
